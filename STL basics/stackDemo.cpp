d9604aae0386ca163910c4c94f1c4900f309589f
Krishnendu Roy <roykrishnenduroy01@gmail.com>
2025-05-04 16:51:04 +0530

    changes done in logic

--- STL basics/stackDemo.cpp
+++ STL basics/stackDemo.cpp
@@ -27,21 +27,21 @@ vector<int>arr{100,80,60,70,60,75,85};
 vector<int>v;
 stack<pair<int,int>>st;
 for(int i=0;i<arr.size();i++){
-    if(st.size()==0) v.push_back(-1);
-    else if(st.size()>0 && st.top().first()>arr[i]) v.push_back(st.top());
-    else if(st.size()>0 && st.top().first()<=arr[i]){
-        while(st.size()>0 && st.top().first()<=arr[i]){
+    if(st.empty()) v.push_back(-1);
+    else if(st.size()>0 && st.top().first>arr[i]) v.push_back(st.top().second);
+    else if(st.size()>0 && st.top().first<=arr[i]){
+        while(st.size()>0 && st.top().first<arr[i]){
             st.pop();
         }
         if(st.size()==0) v.push_back(-1);
         else v.push_back(st.top().second);
     }
-    st.push(arr[i],i);//storing each element after each iteration
+    st.push({arr[i], i});//storing each element after each iteration in pair as it is coded here
 }
 
-for(auto &i:v){
+for(int i=0;i<v.size();i++){
     v[i]=i-v[i];
-    cout<<v[i];
+    cout<<v[i]<<" ";
 }
 
 
